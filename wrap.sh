cab1abe36acc21e78d1760df724b420811595909
thmsbfft <thmsbfft@gmail.com>
2017-10-13 21:32:54 -0700

    wrap script

--- wrap.sh
+++ wrap.sh
@@ -2,6 +2,8 @@
 
 # copy files
 echo "Copying files..."
+curl -O https://raw.githubusercontent.com/thmsbfft/electron-wrap/master/package.json
+curl -O https://raw.githubusercontent.com/thmsbfft/electron-wrap/master/index.js
 
 # replace name in package with current directory name
 name=${PWD##*/}
@@ -10,3 +12,5 @@ sed -i '' "s/electron-wrap/$name/g" package.json
 # install dependencies
 npm i
 
+# package the folder
+npm run package
